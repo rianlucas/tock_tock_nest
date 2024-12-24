2039301d9ce948bc10a7fd5ac0353b87c2d58668
rianlucas <rianluccas08@gmail.com>
2024-12-24 09:26:19 -0300

    feat: add Tailwind CSS integration and  shadcn

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,4 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   devServer: { port: 3001 },
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
 });
