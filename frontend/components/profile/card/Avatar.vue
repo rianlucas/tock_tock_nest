f01a563ec7f84eebaf558e8b5e427c71d339ba7d
rianlucas <rianluccas08@gmail.com>
2025-01-26 20:17:11 -0300

    feat(ui): add profile card components for user information display

---
+++ frontend/components/profile/card/Avatar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Pencil } from 'lucide-vue-next';
+
+</script>
+
+<template>
+  <div class="border rounded-xl p-4 flex justify-between items-center mt-4">
+    <div class="flex gap-4">
+      <Avatar/>
+      <div>
+        <p>Rian Lucas</p>
+        <p class="text-gray-400 text-sm">João Pessoa - PB</p>
+      </div>
+    </div>
+    <Button class="text-sm rounded-lg mr-2" variant="outline"> 
+      Edit
+      <Pencil :size="8"/>
+    </Button>
+  </div>
+</template>
---
+++ frontend/components/profile/card/Base.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Pencil } from 'lucide-vue-next';
+
+interface Props { 
+  title: string,
+  informationFields: { label: string; value: string; }[]
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+  <Card class="mt-4">
+    <CardHeader>
+      <div class="flex justify-between items-center">
+        <CardTitle>
+          {{ title }}
+        </CardTitle>
+        <Button class="text-sm rounded-lg" variant="outline"> 
+          Edit
+          <Pencil :size="8"/>
+        </Button>
+      </div>
+    </CardHeader>
+    <CardContent>
+      <div class="flex flex-col gap-4">
+        <div class="grid grid-cols-2 gap-4" >
+          <div v-for="field in informationFields">
+            <p class="text-gray-400 text-sm">{{ field.label }}</p>
+            <p class="mt-1">{{ field.value }}</p>
+          </div>
+        </div>
+      </div>
+
+    </CardContent>
+  </Card>
+</template>
